0d5fa1950a613ca7b8e8d7a6a70f3f316f7cdff9
Xander <contact@xander479.co.uk>
2020-03-14 10:43:46 +0000

    Create twitch.js
    Waits for a second before showing the redirect link

---
+++ scripts/twitch.js
@@ -0,0 +1,5 @@
+window.onload = setTimeout(showLink, 1000);
+
+function showLink() {
+	document.getElementById("link").innerHTML = "<a href=\"https://twitch.tv/Xander479\">Click here</a> to be redirected.";
+}
