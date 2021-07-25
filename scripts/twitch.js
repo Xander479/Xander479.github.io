73064ccc97d69ac9d39ee9d3811fbedb562450b5
Xander <contact@xander479.co.uk>
2021-07-25 17:27:02 +0100

    use event listener instead of .onload

--- scripts/twitch.js
+++ scripts/twitch.js
@@ -1,4 +1,4 @@
-window.onload = setTimeout(showLink, 1000);
+window.addEventListener("load", setTimeout(showLink, 1000));
 
 function showLink() {
 	document.getElementById("link").innerHTML = "<a href=\"https://twitch.tv/Xander479\">Click here</a> to be redirected.";
