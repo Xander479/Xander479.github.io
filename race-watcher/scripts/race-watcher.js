b01e26e2991ff933eefef710bee16bfc8912ff25
Xander <contact@xander479.co.uk>
2021-09-20 05:47:20 +0100

    Implement WebSocket setup

--- race-watcher/scripts/race-watcher.js
+++ race-watcher/scripts/race-watcher.js
@@ -4,4 +4,23 @@ function main() {
 	for(var len = toHide.length; len > 0; len--) {
 		document.getElementsByClassName('show')[0].className = 'hidden';
 	}
+	
+	// Set up websocket
+	const wsURL = "wss://racetime.gg/ws/race/" + document.getElementById('slug').value;
+	var ws = new WebSocket(wsURL);
+	ws.onopen = function () {
+		console.log("connection opened");
+	};
+	ws.onclose = function () {
+		console.log("connection closed");
+	};
+	
+	ws.onmessage = function (event) {
+		const msg = JSON.parse(event.data);
+		// Only bother doing something if it's a race update
+		if(msg.type == "race.data") {
+			console.log(msg.race);	// For testing purposes
+			// TODO: logic for displaying racer details
+		}
+	};
 }
