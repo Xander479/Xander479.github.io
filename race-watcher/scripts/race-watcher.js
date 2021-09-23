63d5c60e9f16dafc8f45de36d22aa0afcd1d5c48
Xander <contact@xander479.co.uk>
2021-09-23 23:17:18 +0100

    Add racers to html doc when they finish/quit

--- race-watcher/scripts/race-watcher.js
+++ race-watcher/scripts/race-watcher.js
@@ -15,12 +15,70 @@ function main() {
 		console.log("connection closed");
 	};
 	
+	var finished = 0
+	var quit = 0;
 	ws.onmessage = function (event) {
-		const msg = JSON.parse(event.data);
 		// Only bother doing something if it's a race update
-		if(msg.type == "race.data") {
-			console.log(msg.race);	// For testing purposes
-			// TODO: logic for displaying racer details
+		if(JSON.parse(event.data).type == "race.data") {
+			const race = msg.race;
+			console.log(race);	// For testing purposes
+			// Check if anyone's finished or quit/unquit
+			if(race.entrants_count_finished != finished) {
+				// Someone new finished
+				if(race.entrants_count_finished > finished) {
+					playerFinished(race);
+					finished++;
+				}
+				// .undone
+				else {
+					playerUndone(race);
+					finished--;
+				}
+			}
+			else if (race.entrants_count_inactive != quit) {
+				// Someone quit
+				if(race.entrants_count_inactive > quit) {
+					playerQuit(race);
+					quit++;
+				}
+				// .unforfeit
+				else {
+					playerUndone(race);
+					quit--;
+				}
+			}
 		}
 	};
+}
+
+function playerFinished(race) {
+	var newDone = "";
+	for(i = 0; i < race.entrants_count_finished; i++) {
+		var racer = race.entrants[i];
+		if(racer.status != "done") {
+			continue;
+		}
+		newDone = racer.place_ordinal + " " + racer.user.name + " - " + racer.finish_time;
+	}
+	const p = document.createElement("p");
+	p.innerHTML = newDone;
+	document.getElementById("done").appendChild(p);
+}
+
+function playerQuit(race) {
+	var newQuit = "";
+	for(i = 0; i < race.entrants_count_inactive; i++) {
+		var racer = race.entrants[i];
+		if(racer.status != "dnf" || racer.status != "dq") {
+			continue;
+		}
+		newQuit = racer.status + " " + racer.user.name;
+	}
+	const p = document.createElement("p");
+	p.innerHTML = newQuit;
+	document.getElementById("quit").appendChild(p);
+}
+
+function playerUndone(race) {
+	
 }
