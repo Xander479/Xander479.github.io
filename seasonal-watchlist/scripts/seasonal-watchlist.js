016fb518c775a07684d092610eaf8fcad89bca0c
Xander <contact@xander479.co.uk>
2021-12-30 19:56:36 +0000

    Hide user form on button press

--- seasonal-watchlist/scripts/seasonal-watchlist.js
+++ seasonal-watchlist/scripts/seasonal-watchlist.js
@@ -1,3 +1,12 @@
 function main() {
-	console.log('In main...');
+	// Don't do anything if no username entered
+	if (document.getElementById('user').value == '') {
+		return;
+	}
+	
+	// Hide user form
+	var toHide = document.getElementsByClassName('show');
+	for (var i = toHide.length; i > 0; i--) {
+		document.getElementsByClassName('show')[0].className = 'hide';
+	}
 }
