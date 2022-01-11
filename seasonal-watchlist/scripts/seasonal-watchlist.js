56d135bad9e29f0e10f385afe2752a9793b491d2
Xander <contact@xander479.co.uk>
2022-01-11 03:58:20 +0000

    Fix formatting

--- seasonal-watchlist/scripts/seasonal-watchlist.js
+++ seasonal-watchlist/scripts/seasonal-watchlist.js
@@ -1,12 +1,12 @@
 function main() {
-  // Don't do anything if no username entered
-  if (document.getElementById('user').value == '') {
-    return;
-  }
+	// Don't do anything if no username entered
+	if (document.getElementById('user').value == '') {
+		return;
+	}
 
-  // Hide user form
-  var toHide = document.getElementsByClassName('show');
-  for (var i = toHide.length; i > 0; i--) {
-    document.getElementsByClassName('show')[0].className = 'hide';
-  }
+	// Hide user form
+	var toHide = document.getElementsByClassName('show');
+	for (var i = toHide.length; i > 0; i--) {
+		document.getElementsByClassName('show')[0].className = 'hide';
+	}
 }
